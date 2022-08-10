bdf2771b3a63abb144df5664421e7ad65dc9b656
heritier sonde <heritiersonde@gmail.com>
2022-08-10 15:57:09 -0500

    test

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const movieRouter = require("./routes/movieRoute");
+const scheduleRouter = require("./routes/scheduleRoute");
+const userRouter = require("./routes/userRoute");
+const ManageReservationRouter = require("./routes/manageRoute");
+const hallSeatRouter= require("./routes/hallseatRoute");
+const reservationRouter = require("./routes/reservationRoute");
+
+const app = express();
+const mysql = require("mysql2")
+const cors = require("cors");
+const { makeReservation } = require("./controller/reservationController");
+app.use(cors());
+app.use(express.json());
+app.use("/",movieRouter);
+app.use("/",scheduleRouter);
+app.use("/",userRouter);
+app.use("/",hallSeatRouter);
+app.use("/",reservationRouter);
+app.use("/",ManageReservationRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+console.log(`server running on port ${port} .....`)
+})
